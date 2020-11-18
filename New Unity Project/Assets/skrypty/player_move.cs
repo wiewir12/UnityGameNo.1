14e0c8f0cc6b962ea73f3b0a0f53e0c22af68286
serosek123 <74649232+serosek123@users.noreply.github.com>
2020-11-18 10:31:41 +0100

    dmoventplayer

---
+++ New Unity Project/Assets/skrypty/player_move.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move : MonoBehaviour
+{
+    public float playerspeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * playerspeed, Input.GetAxis("Vertical") * Time.deltaTime * playerspeed, 0);
+    }
+}
